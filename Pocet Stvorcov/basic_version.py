17f4fe5a1e5c7a33c3ec083830e03c67bc52144e
FekyDEV <71899549+FekyDEV@users.noreply.github.com>
2022-02-07 18:23:21 +0100

    Update basic_version.py

--- Pocet Stvorcov/basic_version.py
+++ Pocet Stvorcov/basic_version.py
@@ -9,23 +9,27 @@
  * @license MIT
  """
 
+## VSTUP ##
 import turtle
 pero = turtle.Turtle()
 tabula = turtle.Screen()
 
+## KÓD ##
 def stvorec(n):
-    n = int(n)
+    n = int(n) # PREMENA "N" NA ČÍSLO (STRING TO NUMBER)
     for j in range(n): # OPAKOVAŤ N-KRÁT
-        for i in range(4): # ŠTVOREC
+        # ŠTVOREC
+        for i in range(4): # ŠTVOREC (OPAKOVAŤ 4-KRÁT)
             pero.forward(50) # DĹŽKA ŠTVORCA
             pero.left(90) # UHOL ŠTVORCA
         # PRESUN
-        pero.penup()
+        pero.penup() # ZDVIHNÚŤ PERO
         pero.left(180) # OTOČIŤ SA
         pero.forward(70) # DLŽKA ŠTVORCA + 20
         pero.right(180) # OTOČIŤ SA
-        pero.pendown()
+        pero.pendown() # POLOŽIŤ PERO
 
+## VÝSTUP ##
 stvorec(3) # POČET ŠTVORCOV
 
 tabula.mainloop() # PONECHAŤ OKNO OTVORENÉ
